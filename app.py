21c54905e2544353eec57b9cd3a23a7c82be5d10
Johandielangman <jghanekom2@gmail.com>
2024-11-07 20:41:36 +0200

    get cookies

--- app.py
+++ app.py
@@ -1,3 +1,34 @@
 import streamlit as st
+import logging
+
+def get_all_cookies():
+    '''
+    WARNING: This uses unsupported feature of Streamlit
+    Returns the cookies as a dictionary of kv pairs
+    '''
+    from streamlit.web.server.websocket_headers import _get_websocket_headers 
+    from urllib.parse import unquote
+
+    headers = _get_websocket_headers()
+    if headers is None:
+        return {}
+    
+    if 'Cookie' not in headers:
+        return {}
+    
+    cookie_string = headers['Cookie']
+    # A sample cookie string: "K1=V1; K2=V2; K3=V3"
+    cookie_kv_pairs = cookie_string.split(';')
+
+    cookie_dict = {}
+    for kv in cookie_kv_pairs:
+        k_and_v = kv.split('=')
+        k = k_and_v[0].strip()
+        v = k_and_v[1].strip()
+        cookie_dict[k] = unquote(v) #e.g. Convert name%40company.com to name@company.com
+    return cookie_dict
+
 
 st.write(f"Hello worlds!, and {st.secrets['name']}!")
+logging.info("This is a logging test")
+logging.info(f"{get_all_cookies()}")
