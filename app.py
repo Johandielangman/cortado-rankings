74dd21ea44d6ea2b9aa399cc28c0ca09fd25188d
Johandielangman <jghanekom2@gmail.com>
2024-11-07 22:44:53 +0200

    feat: add page configuration and menu items for enhanced user experience

--- app.py
+++ app.py
@@ -3,6 +3,18 @@ import hmac
 import hashlib
 import time
 
+st.set_page_config(
+    page_title="Ex-stream-ly Cool App",
+    page_icon="🧊",
+    layout="wide",
+    initial_sidebar_state="expanded",
+    menu_items={
+        'Get Help': 'https://www.extremelycoolapp.com/help',
+        'Report a bug': "https://www.extremelycoolapp.com/bug",
+        'About': "# This is a header. This is an *extremely* cool app!"
+    }
+)
+
 st.title('Simple User Login App')
 
 
