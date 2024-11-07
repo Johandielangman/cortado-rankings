93a4c1396b437ac811f1bc3e0e68c54ff41decb7
Johandielangman <jghanekom2@gmail.com>
2024-11-07 22:47:30 +0200

    feat: add page configuration and menu items for enhanced user experience

--- app.py
+++ app.py
@@ -4,10 +4,9 @@ import hashlib
 import time
 
 st.set_page_config(
-    page_title="Ex-stream-ly Cool App",
-    page_icon="🧊",
-    layout="wide",
-    initial_sidebar_state="expanded",
+    page_title="Cortado Rating",
+    page_icon="☕",
+    layout="centered",
     menu_items={
         'Get Help': 'https://www.extremelycoolapp.com/help',
         'Report a bug': "https://www.extremelycoolapp.com/bug",
